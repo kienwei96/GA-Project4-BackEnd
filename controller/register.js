5cd8004de3b4130a415a34ffc37bd6503d5f443a
kienwei96 <kien_wei@hotmail.com>
2022-02-15 19:09:47 +0800

    create register api

---
+++ controller/register.js
@@ -0,0 +1,81 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const jwt = require('express-jwt');
+const bcrypt = require('bcrypt');
+const jwtDecode = require('jwt-decode');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = require('../models/User');
+const validateRegisterInput = require('../validation/register');
+
+const { createToken, hashPassword, verifyPassword } = require('../util');
+
+router.get('/test', async (req, res) => {
+  return res.send('register api is work!');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { errors, isValid } = validateRegisterInput(req.body);
+    if (!isValid) {
+      return res.status(400).json({
+        message: errors,
+      });
+    }
+
+    const { name, email } = req.body;
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const userData = {
+      name,
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    };
+
+    const existingEmail = await User.findOne({
+      email: userData.email,
+    }).lean();
+
+    if (existingEmail) {
+      return res.status(400).json({ message: 'Email already exists' });
+    }
+
+    const newUser = new User(userData);
+    const savedUser = await newUser.save();
+
+    if (userData) {
+      const token = createToken(savedUser);
+      const decodedToken = jwtDecode(token);
+      const expiresAt = decodedToken.exp;
+
+      const { name, email, role } = savedUser;
+
+      const userInfo = {
+        name,
+        email,
+        role,
+      };
+
+      return res.json({
+        message: 'User created!',
+        token,
+        userInfo,
+        expiresAt,
+      });
+    } else {
+      return res.status(400).json({
+        message: 'There was a problem creating your account',
+      });
+    }
+  } catch (err) {
+    return res.status(400).json({
+      message: 'There was a problem creating your account',
+    });
+  }
+});
+
+module.exports = router;
