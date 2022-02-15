9934894e1e6b5fa184eb99c590d7bb78b4d0e85e
kienwei96 <kien_wei@hotmail.com>
2022-02-15 14:15:06 +0800

    create login api

---
+++ controller/login.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const jwt = require('express-jwt');
+const jwtDecode = require('jwt-decode');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = require('../models/User');
+const validateLoginInput = require('../validation/login');
+
+const { createToken, hashPassword, verifyPassword } = require('../util');
+const { route } = require('./register');
+
+router.get('/test', async (req, res) => {
+  return res.send('login api is work!');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { errors, isValid } = validateLoginInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      email,
+    }).lean();
+
+    if (!user) {
+      return res.status(403).json({
+        message: 'User not found',
+      });
+    }
+
+    const passwordValid = await verifyPassword(password, user.password);
+
+    if (passwordValid) {
+      const { password, ...rest } = user;
+      const userInfo = Object.assign({}, { ...rest });
+
+      const token = createToken(userInfo);
+
+      const decodedToken = jwtDecode(token);
+      const expiresAt = decodedToken.exp;
+
+      res.json({
+        message: 'Authentication successful!',
+        token,
+        userInfo,
+        expiresAt,
+      });
+    } else {
+      res.status(403).json({
+        message: 'Wrong password.',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: 'Something went wrong.' });
+  }
+});
+
+module.exports = router;
