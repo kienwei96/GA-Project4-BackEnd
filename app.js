2acb3ca6057d7fdb1f811e2ba7b4552182bc1146
kienwei96 <kien_wei@hotmail.com>
2022-02-15 14:03:19 +0800

    setup routes

--- app.js
+++ app.js
@@ -28,30 +28,40 @@ app.use(
 // static files middleware
 app.use(express.static('public'));
 
+// test api
+app.get('/api', async (req, res) => {
+  return res.send('api is work!');
+});
+
 //CONTROLLERS
 // login controller
-const loginController = require('./controller/authenticate');
-app.use('/api/authenticate', loginController);
+
+const loginController = require('./controller/login');
+app.use('/api/login', loginController);
+
+// register controller
+const registerController = require('./controller/register');
+app.use('/api/register', registerController);
 
 // attach user to the Request object/api
-const attachUser = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) {
-    return res.status(401).json({ message: 'Authentication invalid' });
-  }
+// const attachUser = (req, res, next) => {
+//   const token = req.headers.authorization;
+//   if (!token) {
+//     return res.status(401).json({ message: 'Authentication invalid' });
+//   }
 
-  const decodedToken = jwtDecode(token.slice(7));
+//   const decodedToken = jwtDecode(token.slice(7));
 
-  if (!decodedToken) {
-    return res
-      .status(401)
-      .json({ message: 'There was a problem authorizing the request' });
-  } else {
-    req.user = decodedToken;
-    next();
-  }
-};
+//   if (!decodedToken) {
+//     return res
+//       .status(401)
+//       .json({ message: 'There was a problem authorizing the request' });
+//   } else {
+//     req.user = decodedToken;
+//     next();
+//   }
+// };
 
-app.use(attachUser);
+// app.use(attachUser);
 
 module.exports = app;
